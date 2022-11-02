5a96e99bd2488868acfb9f6af8b7102b087bc9d8
SakovichElena <lenkasakovich.le@gmail.com>
2022-11-02 23:28:23 +0300

    Add index.js

---
+++ index.js
@@ -0,0 +1,89 @@
+"use strict";
+let x = 20;
+let y = 58;
+let z = 42;
+console.log(x + y + z);
+// 
+
+let seconds = 60;
+let minutes = 60;
+let hours = 24;
+let days = 365;
+let myAgeInSeconds = 22;
+alert (seconds )
+alert (seconds * minutes * hours * days * myAgeInSeconds);
+// 
+
+let count = 42;
+let userName = '42';
+count = '42'
+userName = 42
+alert(count , userName);
+// 
+let count = 42;
+let userName = '42';
+count = String(count);
+typeof(count);
+userName = Number(userName);
+typeof(userName);
+// 
+
+let a = 1;
+let b = 2;
+let c = "белых медведей";
+a = "1";
+b = "2";
+c = "белых медведей";
+console.log(a + b + ' ' + c);
+// 
+
+let a = "доступ";
+let b = "морпех";
+let c = "наледь";
+let d = "попрек";
+let e = "рубило";
+let lengthWords;
+lengthWords = (a + b + c + d + e);
+lengthWords.length;
+// 
+
+let favoriteColor = "lilac";
+let herAge = 53;
+let hisSchedule = null;
+`Variable:favoriteColor have type:String`
+`Variable:herAge have type:Number`
+`Variable:hisSchedule have type:Null`
+// 
+
+let userName = "Please enter your name";
+console.log(userName);
+let userAge = "Please enter your age";
+console.log(userAge);
+// 
+let userInfo = ["userName", "userAge"];
+console.log(userInfo[0] = "Please enter your name");
+console.log(userInfo[1] = "Please enter your age");
+// 
+
+let a = 4;
+let b = 3;
+console.log(a,b);
+a = a^b;
+b = a^b; 
+a = a^b;
+console.log(a,b);
+// 
+
+let codeWord1 = "обернись";
+console.log(codeWord1[1]);
+let codeWord2 = "неужели";
+console.log(codeWord2[1]);
+let codeWord3 = "огурцы";
+console.log(codeWord3[1]);
+let codeWord4 = "липкие";
+console.log(codeWord4[1]);
+let cipher = " codeWord1 + codeWord2 + codeWord3 + codeWord4";
+// 
+
+
+
