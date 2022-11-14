1fb670d143d69bec020945faca1dc2f232d9ca96
SakovichElena <lenkasakovich.le@gmail.com>
2022-11-14 21:47:56 +0300

    Add new tasks

--- index.js
+++ index.js
@@ -712,6 +712,19 @@ if (login == "Expecto Patronum") {
 }
 // 
 
+//  task 7 
+{
+let student1 = {
+    name: 'Polina',
+    age: 27,
+}
+let student2 = {
+    name: 'Polina',
+    age: 27,
+}
+console.log(JSON.stringify(student1) === JSON.stringify(student2));
+}
+// 
 
 
 
